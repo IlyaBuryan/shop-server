fb2aaa1e859ac20ac946a0cd145ecd540ddc05d0
Ilya <forwow21@mail.ru>
2021-04-20 11:58:31 +0300

    initial

--- geekshop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-import mainapp.views as mainapp
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', mainapp.index, name='index'),
-    path('products/', mainapp.products, name='products'),
-]
