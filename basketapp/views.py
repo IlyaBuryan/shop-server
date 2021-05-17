bf67853f9be7d1e06678cb504970a8291be59cab
Ilya <forwow21@mail.ru>
2021-05-17 10:16:22 +0300

    Lesson6_HW - AJAX

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
+from django.template.loader import render_to_string
 
 from .models import Basket
 from mainapp.models import Product
@@ -24,3 +25,20 @@ def basket_add(request, product_id=None):
 def basket_remove(request, id):
     Basket.objects.get(id=id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, id, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+        baskets = Basket.objects.filter(user=request.user)
+        content = {'baskets': baskets}
+        result = render_to_string('basketapp/basket.html', content)
+
+        return JsonResponse({'result': result})
